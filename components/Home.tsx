7776ad407150ff89530aa4065c485a0ce8c6a046
Luciano Mammino <luciano.mammino@fourtheorem.com>
2022-07-20 09:03:37 +0100

    first commit

---
+++ components/Home.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import useInvite from './hooks/useInvite'
+import Pizza from './images/Pizza'
+import styles from './Home.module.css'
+
+export default function Home () {
+  const [invite, error] = useInvite()
+
+  if (error) {
+    return <div>{error}</div>
+  }
+
+  if (!invite) {
+    return <Pizza className="spin" width={200}/>
+  }
+
+  return (
+    <>
+      <Pizza width={200}/>
+      <h1 className={styles.title}>Secret Pizza Party!</h1>
+      <h2 className={styles.subtitle}>Dec 31st 2022 - 122 and 1/8th, New York City</h2>
+
+      <div className={styles.card} style={{ borderColor: invite.color }}>
+        <h3>Hello, <strong>{invite.name}</strong>!</h3>
+        <p>You have been invited to the most awesome secret pizza party of the year!</p>
+        <fieldset className={styles.fieldset} onChange={(e) => console.log((e.target as HTMLInputElement).value, invite.code)}>
+          <legend>Are you coming?</legend>
+
+          <label htmlFor="yes">
+            <input type="radio" id="yes" name="coming" value="yes"/>
+            Cowabunga! (yes)
+          </label>
+
+          <label htmlFor="no">
+            <input type="radio" id="no" name="coming" value="no"/>
+            Nitwits! (no)
+          </label>
+        </fieldset>
+        <p>
+          <small>
+            <strong>P.S.</strong> You don&apos;t need to bring your <strong>{invite.weapon}</strong>, <em>Shredder</em> is not invited!
+          </small>
+        </p>
+      </div>
+    </>
+  )
+}
---
+++ components/hooks/useInvite.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+
+function delay (ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+interface Invite {
+  code: string,
+  name: string,
+  color: string,
+  weapon: string,
+  coming?: boolean,
+}
+
+export default function useInvite (): [Invite | null, string | null] {
+  const [invite, setInvite] = useState<Invite | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await delay(2000)
+        const invite = {
+          // b0cbb4a4-8a31-4bc1-bee9-d6fe39c1a6b3	Donatello	purple	Bo
+          code: 'ce7d5886-2166-4aee-8038-548f68b9739d',
+          name: 'Donatello',
+          color: 'purple',
+          weapon: 'Bo'
+        }
+        setInvite(invite)
+      } catch (error) {
+        setError((error as Error).message)
+      }
+    }
+    init()
+  }, [])
+
+  return [invite, error]
+}
