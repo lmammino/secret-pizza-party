57801f82545cdad1d0d943d1cdb75a6f6172a965
Luciano Mammino <lucianomammino@gmail.com>
2022-08-05 19:54:03 +0100

    comments are good

--- utils/airtable.ts
+++ utils/airtable.ts
@@ -1,16 +1,20 @@
 import Airtable from 'airtable'
 import { Invite } from '../types/invite'
 
+// make sure all the necessary env vars are set
 if (!process.env.AIRTABLE_API_KEY) {
   throw new Error('AIRTABLE_API_KEY is not set')
 }
-
 if (!process.env.AIRTABLE_BASE_ID) {
   throw new Error('AIRTABLE_BASE_ID is not set')
 }
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID)
+// create a new Airtable client and gets a reference to the
+// airtable base containing our invites
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+const base = airtable.base(process.env.AIRTABLE_BASE_ID)
 
+// function used to "sanitize" query data and avoid injections
 function escape (value: string): string {
   if (value === null || typeof value === 'undefined') {
     return 'BLANK()'
@@ -37,26 +41,39 @@ function escape (value: string): string {
   throw Error('Invalid value received')
 }
 
+// get an invite by invite code (promisified)
 export function getInvite (inviteCode: string): Promise<Invite> {
   return new Promise((resolve, reject) => {
-    base('invites').select({ filterByFormula: `invite = ${escape(inviteCode)}` }).firstPage((err, records) => {
-      if (err) {
-        console.error(err)
-        return reject(err)
-      }
+    base('invites')
+      // runs a query on the `invites` table
+      .select({
+        filterByFormula: `{invite} = ${escape(inviteCode)}`,
+        maxRecords: 1
+      })
+      // reads the first page of results
+      .firstPage((err, records) => {
+        if (err) {
+          // propagate errors
+          console.error(err)
+          return reject(err)
+        }
 
-      if (!records || records.length === 0) {
-        return reject(new Error('Invite not found'))
-      }
+        // if the record could not be found
+        // we consider it an error
+        if (!records || records.length === 0) {
+          return reject(new Error('Invite not found'))
+        }
 
-      const result = {
-        code: String(records[0].fields.invite),
-        name: String(records[0].fields.name),
-        favouriteColor: String(records[0].fields.favouriteColor),
-        weapon: String(records[0].fields.weapon)
-      }
+        // otherwise we create an invite object from the first record
+        // (there should be only one with the give code) and return it
+        const result = {
+          code: String(records[0].fields.invite),
+          name: String(records[0].fields.name),
+          favouriteColor: String(records[0].fields.favouriteColor),
+          weapon: String(records[0].fields.weapon)
+        }
 
-      resolve(result)
-    })
+        resolve(result)
+      })
   })
 }
